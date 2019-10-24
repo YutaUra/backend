55f2418fde4ec9654601c166ee43af9787644b81
yuuta3594 <yuuta3594@outlook.jp>
2019-10-24 10:40:18 +0900

    fix get session view

--- study/urls.py
+++ study/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('get/textbook/<subject>/', v.TextbookGetView.as_view()),
     path('get/unit/', v.TextbookUnitGetView.as_view()),
     path('get/chapter/', v.TextbookChapterGetView.as_view()),
-    path('get/session/', v.get_word_session),
+    path('get/session/<pub_id>/', v.get_word_session),
     path('word/', include('study.words.urls'))
 ]
--- study/views/session.py
+++ study/views/session.py
@@ -1,14 +1,17 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import F
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
 
+from users.models import User
+
 
 @api_view(http_method_names=('GET',))
 @permission_classes(permission_classes=(IsAuthenticated,))
-def get_word_session(request):
-    user = request.user
+def get_word_session(request, pub_id):
+    user = get_object_or_404(User, pub_id=pub_id)
     sessions = user.studysession_set.order_by('-created_at')[:5]
     data = []
     for session in sessions:
